bebf567e504b774c44d218ce252ce203a5b6dd70
Jonah Cohen <yonahchn@gmail.com>
2025-08-07 19:26:49 -0400

    Update linux-server-to-azure.sh

--- Logging & Monitoring/linux-server-to-azure.sh
+++ Logging & Monitoring/linux-server-to-azure.sh
@@ -4,6 +4,7 @@ year=`date '+%Y'`;
 month=`date '+%m'`;
 day=`date '+%d'`;
 hour=`date '+%H'`;
+
 dir='/home/syslog/log/messages';
 #  In SAS add /log/$year/$month/$day/$hour after https://storageaccountname.blob.core.windows.net
 sig='<ENTER SECURE ACCESS SIGNATURE';
